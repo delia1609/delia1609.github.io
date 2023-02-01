6385c85c927710beb54e695694d10a3d5c70a63b
Delia Cristescu <delia.cristescu93@gmail.com>
2023-02-01 12:20:22 +0200

    Add routing and sample routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { CurrentProductProvider } from "./context/CurrentProductContext";
 import { CartContextProvider } from "./context/CartContext";
 import ProductsPage from "./pages/ProductsPage";
 import CartStatus from "./components/CartStatus";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import User from "./components/User";
+import Layout from "./components/Layout";
+import UserLayout from "./components/UserLayout";
 
 const StyledApp = styled.div`
   display: flex;
@@ -17,12 +23,29 @@ function App() {
 
   return (
     <StyledApp>
-      <CartContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home /> } />
+
+          <Route path="about" element={<About />} />
+          <Route path="user" element={<UserLayout />}> 
+            <Route index element={<div>Users List</div>} />
+            <Route path="featured" element={<div>Featured Users</div>}/>
+            <Route path=":userId" element={<User />}/>
+          </Route>
+        </Route>
+        {/* {/* <Route path="about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="user/:userId" element={<User />} />
+        <Route path="account/:accountId/:userId" element={<User />} />
+        <Route path="*" element={<div>Page Not Found</div>} /> */}
+      </Routes>
+      {/* <CartContextProvider>
         <CartStatus />
         <CurrentProductProvider>
           <ProductsPage />
         </CurrentProductProvider>
-      </CartContextProvider>
+      </CartContextProvider> */}
     </StyledApp>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,37 @@
+import { Link, NavLink, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+margin-top: 10px;
+`
+const Nav = styled.nav`
+display: flex;
+gap: 10px;
+`
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: #176ad1;
+
+  &.active {
+    color: firebrick;
+  }
+`
+
+export default function Layout() {
+  return (
+    <div>
+      <Nav>
+        <StyledNavLink to="/" end>Home</StyledNavLink>
+        <StyledNavLink to="about" end>About</StyledNavLink>
+        <StyledNavLink to="user" end>Users List</StyledNavLink>
+        <StyledNavLink to="user/featured" end>Featured Users</StyledNavLink>
+        <StyledNavLink to="user/1" end>User 1</StyledNavLink>
+      </Nav>
+
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+  );
+}
